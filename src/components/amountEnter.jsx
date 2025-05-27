a325c330550436351ecc81048407edbae5f90e2a
Shubhrati patel <shubhratipatel03@gmail.com>
2025-05-28 01:49:53 +0530

    Amount Span Corrected- made flex

--- src/components/amountEnter.jsx
+++ src/components/amountEnter.jsx
@@ -6,7 +6,7 @@ function AmountEnter({onChange,Amount,sym}) {
     bg-white hover:bg-gray-100 px-4 py-2 text-2xl rounded-2xl flex 
     flex-col justify-center'>
       <label className="text-sm font-normal text-zinc-400">Amount</label>
-      <span className="text-black" >
+      <span className="text-black flex" >
         {sym}
         <input 
             className="text-md font-semibold text-black ml-3" 
